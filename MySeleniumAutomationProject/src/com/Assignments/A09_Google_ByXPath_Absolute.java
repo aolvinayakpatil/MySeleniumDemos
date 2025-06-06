050b81d928d62000a7d5e4747ee1d5f091f79a1d
Vinayak <aolvinayakpatil@gmail.com>
2025-06-07 00:33:31 +0530

    Assignment 8 & 9 completed

---
+++ MySeleniumAutomationProject/src/com/Assignments/A09_Google_ByXPath_Absolute.java
@@ -0,0 +1,20 @@
+package com.Assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A09_Google_ByXPath_Absolute {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get( "https://www.google.com");
+		WebElement srch = driver.findElement(By.xpath("html/body/div/div[3]/form/div/div/div/div/div[2]/textarea"));
+		srch.sendKeys("Pattankodoli Yatra");
+		srch.sendKeys(Keys.ENTER);
+	}
+}
