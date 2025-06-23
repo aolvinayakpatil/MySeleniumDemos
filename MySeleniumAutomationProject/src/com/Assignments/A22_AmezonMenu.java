26b89f17bbb2579cdb4f19840767352fbe9ff6da
aolvinayakpatil <aolvinayakpatil@gmail.com>
2025-06-23 23:35:28 +0530

    wip assignment-22

---
+++ MySeleniumAutomationProject/src/com/Assignments/A22_AmezonMenu.java
@@ -0,0 +1,17 @@
+package com.Assignments;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A22_AmezonMenu {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+	}
+
+}
